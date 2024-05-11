334ee87170a6859575821e908bf325e39af0ebf4
Shraddha Kulkarni <shraddhakulkarni@Shraddhas-Air.hsd1.ca.comcast.net>
2024-05-10 22:39:54 -0700

    Project change

---
+++ src/main/java/Constants/Projconstants.java
@@ -0,0 +1,13 @@
+package Constants;
+
+import java.sql.Connection;
+
+public class Projconstants {
+	
+	
+	public static final String page1 = "Views/SuccessPage.jsp";
+	public static final String page2 = "Views/LoginPage.jsp";
+	public static final String page3 = "Views/Error.jsp";
+	public static final String page4 = "Views/Success.jsp";
+
+}
